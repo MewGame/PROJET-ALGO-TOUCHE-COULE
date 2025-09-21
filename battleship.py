43a6d8a66e6e4210a3392985849015ffdada2e51
MewGame <poke.brones@gmail.com>
2025-09-21 23:20:09 +0200

    fix: utiliser place_fleet([3,2,1])

--- battleship.py
+++ battleship.py
@@ -1,9 +1,11 @@
 # battleship.py
+# Commit 7 : on place enfin les 3 bateaux (1,2,3 cases)
 
 import random
 SIZE = 6
 
-def make_grid(n): return [["." for _ in range(n)] for _ in range(n)]
+def make_grid(n): 
+    return [["." for _ in range(n)] for _ in range(n)]
 def show(g, reveal=False):
     print("  A B C D E F")
     for i, row in enumerate(g, start=1):
@@ -17,7 +19,7 @@ def place_ship_horiz(g, L):
         g[r][c+i] = "S"
 
 def place_fleet(g, sizes):
-    for L in sizes:
+    for L in sizes: 
         place_ship_horiz(g, L)
 
 def parse_coord(t):
@@ -28,42 +30,44 @@ def parse_coord(t):
             return int(a)-1,int(b)-1
     return int(t[1:])-1, ord(t[0])-ord("A")
 
-def shoot(g, r, c):
-    if not (0 <= r < SIZE and 0 <= c < SIZE): 
+def shoot(g,r,c):
+    if not (0<=r<SIZE and 0<=c<SIZE): 
         return "out"
-    if g[r][c] == "S": 
-        g[r][c] = "X"; 
+    if g[r][c]=="S": 
+        g[r][c]="X"; 
         return "hit"
-    if g[r][c] == ".": 
-        g[r][c] = "o"; 
+    if g[r][c]==".": 
+        g[r][c]="o"; 
         return "miss"
     return "repeat"
 
-def all_sunk(g):
-    return all(cell != "S" for row in g for cell in row)
+def all_sunk(g): 
+    return all(cell!="S" for row in g for cell in row)
 
 def main():
     g = make_grid(SIZE)
-    place_ship_horiz(g, 3)
+    place_fleet(g, [3,2,1])
+    print("Flotte: 3,2,1 cases. +2 hit, -1 miss, +3 bonus fin.")
     show(g)
     score = 0
     while True:
-        if all_sunk(g):
-            score += 3; print("Victoire ! Score final:", score);
-            show(g, True); 
+        if all_sunk(g): 
+            score+=3; 
+            print("Victoire ! Score final:",score); 
+            show(g,True); 
             break
-        s = input("Tir: ")
+        s=input("Tir: ")
         if not s.strip(): 
-            print("Fin. Score:", score); 
-            show(g, True); 
+            print("Fin. Score:",score); 
+            show(g,True); 
             break
-        r, c = parse_coord(s)
-        res = shoot(g, r, c)
-        if res == "hit": 
-            score += 2
-        elif res == "miss": 
-            score -= 1
-        print("Score:", score); 
+        r,c = parse_coord(s)
+        res = shoot(g,r,c)
+        if res=="hit": 
+            score+=2
+        elif res=="miss": 
+            score-=1
+        print("Score:",score); 
         show(g)
 
 if __name__ == "__main__":
